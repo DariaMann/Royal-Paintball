c8b142126be36128411336cc527172030b39508f
DariaMann <Dasaman60@gmail.com>
2019-03-01 22:36:54 +0300

    message

---
+++ Внешняя часть/Royal Paintball/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+
+public class NetworkManager : MonoBehaviour {
+
+    private ClientTCP clientTCP = new ClientTCP();
+    [SerializeField]
+    public GameObject playerPref;
+    private Dictionary<int, GameObject> playerList = new Dictionary<int, GameObject>();
+    public int MyIndex=0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void Start () {
+        clientTCP.Connect();
+     //   clientTCP.GetPos();
+       // Pos(clientTCP.GetPos());
+        InstantiatePlayer(MyIndex, clientTCP.GetPos());
+    }
+    public void Pos(string str)
+    {
+        string[] words = str.Split(new char[] { ',' });
+        float x= Convert.ToSingle(words[0]);
+        float y= Convert.ToSingle(words[1]); 
+        float z= Convert.ToSingle(words[2]);
+        Vector3 v = new Vector3(x, y, z);
+        playerPref.transform.position=v;
+    }
+    private void Update()
+    {
+       // clientTCP.Message(MyIndex);
+        //clientTCP.Place(playerPref);
+    }
+    public void InstantiatePlayer(int index, string str)
+    {
+        string[] words = str.Split(new char[] { ',' });
+        float x = Convert.ToSingle(words[0]);
+        float y = Convert.ToSingle(words[1]);
+        float z = Convert.ToSingle(words[2]);
+        Vector3 v = new Vector3(x, y, z);
+      //  playerPref.transform.position = v;
+        GameObject temp = Instantiate(playerPref,v, Quaternion.identity);
+        temp.name = "Player: " + index;
+        playerList.Add(index, temp);
+        MyIndex++;
+    }
+
+}
---
+++ Внешняя часть/Royal Paintball/Assets/Scripts/PlayerKeyboardController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKeyboardController : MonoBehaviour {
+
+    float XSize = 9f;
+    float ZSize = 9f;
+    [SerializeField]
+    GameObject foodPrefab;
+    GameObject curFood;
+    Vector3 curPos;
+    [SerializeField]
+    int countFood;
+
+    float x;
+    float z;
+    public GameObject Plane;
+
+
+    public float seconds = 1f;
+    public float timer = 10f;
+
+    public void Start()
+    {
+        //AddNewFood();
+        x = Plane.transform.position.x;
+        z = Plane.transform.position.z;
+    }
+    void AddNewFood()//Добавление еды
+    {
+        countFood = Random.Range(7, 20);
+        int j = countFood;
+        for (int i = 0; i < j; i++)
+        {
+            RandomPos();
+            curFood = GameObject.Instantiate(foodPrefab, curPos, Quaternion.identity) as GameObject;
+            countFood++;
+        }
+    }
+
+    void RandomPos()//Рандомная позиция
+    {
+        curPos = new Vector3(Random.Range(z * -1, z), 0.25f, Random.Range(x * -1, x));
+    }
+}
